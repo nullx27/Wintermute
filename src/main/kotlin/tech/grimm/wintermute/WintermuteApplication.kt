58e16f6ffe3ca661b39f382e6fc92e8559f5bf4e
Andre Peiffer <necrotex@gmail.com>
2022-06-16 18:04:06 +0200

    Added Heartbeat to keep the application running

--- src/main/kotlin/tech/grimm/wintermute/WintermuteApplication.kt
+++ src/main/kotlin/tech/grimm/wintermute/WintermuteApplication.kt
@@ -5,20 +5,24 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
-
+import org.springframework.scheduling.annotation.Scheduled
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
 @ComponentScan(
-    *[
-        "tech.grimm.wintermute.components",
-        "tech.grimm.wintermute.interactions",
-        "tech.grimm.wintermute.handlers",
-        "tech.grimm.wintermute.services"
-    ]
+    "tech.grimm.wintermute.components",
+    "tech.grimm.wintermute.interactions",
+    "tech.grimm.wintermute.handlers",
+    "tech.grimm.wintermute.services"
 )
-class WintermuteApplication
+class WintermuteApplication {
+    /**
+     * Heartbeat to keep the Application running and responsive
+     */
+    @Scheduled(fixedRate = 5000)
+    fun heartbeat() {}
+}
 
 fun main(args: Array<String>) {
     runApplication<WintermuteApplication>(*args)
